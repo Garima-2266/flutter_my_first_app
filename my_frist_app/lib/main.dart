6507b148957d4d0001b93622e22e2f6ff6b55227
Garima <garima2266@gmail.com>
2023-06-01 03:17:59 +0545

    hh

---
+++ my_frist_app/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          elevation: 0.0,
+          leading: Icon(
+            Icons.arrow_back,
+            // color: Colors.red,
+          ),
+          title: Text(
+            "Back to Shopping",
+            // style: TextStyle(
+            //   color: Colors.green,
+            //   fontSize: 14,
+            // ),
+          ),
+          actions: [
+            Icon(
+              Icons.favorite_border_outlined,
+            ),
+          ],
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Image.network(
+                "https://images.pexels.com/photos/1926404/pexels-photo-1926404.jpeg?cs=srgb&dl=pexels-imlfernandez-1926404.jpg",
+                width: MediaQuery.of(context).size.width,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
